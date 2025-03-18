5633f22922b6abc142ceadccf22831bb8b2a27b3
adr0179 <143733322+adr0179@users.noreply.github.com>
2025-03-18 01:00:51 -0400

    Delete C++/Art directory

--- C++/Art/visualarts.cpp
+++
@@ -1,9 +0,0 @@
-#include "type.cpp"
-
-class Painting : public VisualArt {
-    private:
-        int size;
-    public:
-        Painting(string a, string t, int y, string ty, int s) : VisualArt(a, t, y, ty) {size = s;}
-        void showCasePaintintg() {cout << "This is the " << title << " painted by " << artist << " in " << year << endl;}
-};
